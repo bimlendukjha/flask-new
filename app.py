88259ad4f50882c674ad114bd693785d9d4670ff
Bimlendu Kumar Jha <bimlendukjha@gmail.com>
2024-10-27 17:05:47 +0530

    completed

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "hello, world"
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
+    return render_template("index.html")
+
            
